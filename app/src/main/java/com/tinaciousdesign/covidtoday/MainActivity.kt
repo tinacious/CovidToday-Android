0c8f3aaf93b8c5550a70f6779fa86feb8c4e9915
Tina Holly <info@tinaciousdesign.com>
2021-01-02 20:06:12 -0800

    open readme in new window

--- app/src/main/java/com/tinaciousdesign/covidtoday/MainActivity.kt
+++ app/src/main/java/com/tinaciousdesign/covidtoday/MainActivity.kt
@@ -1,12 +1,14 @@
 package com.tinaciousdesign.covidtoday
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuInflater
 import android.view.MenuItem
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
-import androidx.fragment.app.FragmentActivity
 import androidx.viewpager2.widget.ViewPager2
 import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayoutMediator
@@ -50,14 +52,22 @@ class MainActivity : AppCompatActivity() {
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         when (item.itemId) {
-            R.id.menu_help -> handleHelpClicked()
+            R.id.menu_about -> handleAboutClicked()
             else -> return super.onOptionsItemSelected(item)
         }
         return true
     }
 
-    private fun handleHelpClicked() {
-
+    private fun handleAboutClicked() {
+        AlertDialog.Builder(this)
+            .setTitle(R.string.info_open_title)
+            .setMessage(R.string.info_open_description)
+            .setPositiveButton(android.R.string.ok) { _, _ ->
+                val webIntent = Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/tinacious/CovidToday-Android"))
+                startActivity(webIntent)
+            }
+            .setNegativeButton(android.R.string.cancel, null)
+            .show()
     }
 
     companion object {
